904a21f80561d22f88dbc1f14bb9d81c536132c1
Costi <sova.victor98@gmail.com>
2022-09-09 11:48:40 +0300

    Exercitii proiect masina

---
+++ CarExercisesApp/CarExercisesApp/Car.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarExercisesApp
+{
+    internal class Car
+    {
+        private const int codCalculSerieMotor = 5;
+        private static string codSerieSasiu;
+        private static string brand;
+
+        private readonly long serieSasiu;
+        private readonly long serieMotor;
+        private string model;
+        private string color;
+
+        //Constructor implicit
+        public Car(int serieSasiu)
+        {
+            this.serieSasiu = serieSasiu;
+            this.serieMotor = initCodSerieMotor();
+        }
+        //Constructor explicit partial
+        public Car(string model, long serieSasiu)
+        {
+            this.model = model;
+            this.serieSasiu = serieSasiu;
+        }
+        //Constructor explicit
+        public Car(long serieSasiu, long serieMotor, string model, string color)
+        {
+            this.serieSasiu=serieSasiu;
+            this.serieMotor = initCodSerieMotor();
+            this.model=model;
+            this.color=color;
+        }
+        //Setter & getter model
+        public string getModel()
+        {
+            return this.model;
+        }
+        public void setModel(string model)
+        {
+            this.model = model;
+        }
+        //Setter & getter brand
+        public string getBrand()
+        {
+            return Car.brand;
+        }
+        public static void setBrand(string brand)
+        {
+            Car.brand = brand;
+        }
+        //Metoda 
+        private long initCodSerieMotor()
+        {
+            return serieSasiu * codCalculSerieMotor;
+        }
+        
+       
+    }
+}
---
+++ CarExercisesApp/CarExercisesApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CarExercisesApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Car car1 = new Car(12345);
+            car1.setModel("A5");
+            Console.WriteLine("Model: " + car1.getModel());
+
+            Car car2 = new Car("x6", 333221);
+            Car.setBrand("BMW");
+            Console.WriteLine("Brand: " + car2.getBrand());
+
+
+
+
+        }
+    }
+}
